4f62ea208ca072a83498ba58e2c2acfa12c4592c
ChibiaSad <magur1997@gmail.com>
2022-09-20 18:15:34 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/Homework2d5Application.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework2d5Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Homework2d5Application.class, args);
+    }
+}
